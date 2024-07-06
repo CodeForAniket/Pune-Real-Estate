fce1e0d70ec09c8e9349da362cec841f5963ee15
Aniket Patil <114733682+CodeForAniket@users.noreply.github.com>
2024-07-07 00:59:04 +0530

    Its completed

---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1720294048/wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://codeforaniket.github.io/Pune-Real-Estate/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fpune-real-estate%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D3679&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://codeforaniket.github.io/Pune-Real-Estate/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fpune-real-estate%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D3679&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://codeforaniket.github.io/Pune-Real-Estate/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fpune-real-estate%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D3679&reauth=1">https://codeforaniket.github.io/Pune-Real-Estate/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fpune-real-estate%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D3679&reauth=1</a></p>
+	</body>
+</html>
